5dde472179553e948692dc72185099f85ceda428
Marco Adrián Domínguez Jiménez <l20140892@queretaro.tecnm.mx>
2024-02-29 12:32:48 -0600

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -27,3 +28,5 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, bootstrap');
   });
 });
+
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +11,47 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'bootstrap';
+
+sound(numero: number): void {
+  let audio = new Audio();
+  switch (numero)
+  {
+    case 1: audio.src = 'assets/sounds/piano_1.mp3';
+    audio.play();
+    audio.play(); break;
+    case 2: audio.src = 'assets/sounds/alfedo-calmate.mp3';
+    audio.play();
+    audio.play();
+    break;
+    case 3:
+    audio.src = 'assets/sounds/siniestro-dross-negas.mp3';
+    audio.play();
+    audio.play();
+    break;
+    case 4:
+    audio.src = 'assets/sounds/hola-tonotos.mp3';
+    audio.play();
+    audio.play();
+    break;
+    case 5:
+    audio.src = 'assets/sounds/y2mate_Ajp9shj.mp3';
+    audio.play();
+    audio.play();
+    break;
+    case 6:
+    audio.src = 'assets/sounds/morbido.mp3';
+    audio.play();
+    audio.play();
+    break; case 7:
+    audio.src = 'assets/sounds/y2mate_Ajp9shj.mp3';
+    audio.play();
+    audio.play();
+    break;
+    default:
+    audio.src = 'assets/sounds/hola-tonotos.mp3';
+    audio.play();
+    audio.play();
+    break;
+  }
+  }
 }
